81464c3f097f1347f2c5b21ffe8c4b5f96d23670
DaiQiangReal <daiqianghaha@foxmail.com>
2020-06-02 16:51:18 +0800

    remove useless Files

---
+++ src/components/BlogsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      List
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name:"BlogsList",
+            components:{
+
+            },
+        }
+    },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    },
+    
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>>
